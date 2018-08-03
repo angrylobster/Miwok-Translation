f7d99ef945b1831d2b504e09809da729399fca3c
angrylobster <liewminshan@gmail.com>
2018-08-03 17:14:38 +0800

    Add sound functionality for PhrasesActivity

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,17 +5,20 @@ public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
     private int mImageResourceID;
+    private int mAudioResourceID;
 
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int mAudioResourceID) {
         this.mDefaultTranslation = defaultTranslation;
         this.mMiwokTranslation = miwokTranslation;
+        this.mAudioResourceID = mAudioResourceID;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int mImageResourceID) {
+    public Word(String defaultTranslation, String miwokTranslation, int mImageResourceID, int mAudioResourceID) {
         this.mDefaultTranslation = defaultTranslation;
         this.mMiwokTranslation = miwokTranslation;
         this.mImageResourceID = mImageResourceID;
+        this.mAudioResourceID = mAudioResourceID;
     }
 
     public int getmImageResourceID() {
@@ -32,5 +35,9 @@ public class Word {
 
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getmAudioResourceID() {
+        return mAudioResourceID;
     }
 }
